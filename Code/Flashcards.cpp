b6860a5d8f94c4e8640ba247df8fd33ceb69a335
ef1500 <45581646+ef1500@users.noreply.github.com>
2019-09-19 11:08:10 -0600

    Added Code Folder and Some Code Files

---
+++ Code/Flashcards.cpp
@@ -0,0 +1,21 @@
+// +----------------+--------+
+// |   Mnexacore    | ef1500 |
+// +----------------+--------+
+// |      Flashcard Code     |
+// +----------------+--------+
+
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+class Flashcard
+	public:
+
+		//Declaring the different parts of the flashcards
+		string Question;
+		string Answer;
+		string image;
+
+		int ID;
---
+++ Code/dbIntegration.cpp
@@ -0,0 +1,10 @@
+// +---------------------------+--------+
+// |         Mnexacore         | ef1500 |
+// +---------------------------+--------+
+// |     Database Integration Code      |
+// +---------------------------+--------+
+
+#include <cstdio>
+#include <sqlite3.h>
+
+//So far, I don't know how to do this, which is epic
