c4a104683b7cfed077d4274f5a10fd887c72b2e9
ef1500 <45581646+ef1500@users.noreply.github.com>
2020-01-19 16:55:01 -0700

    Added some more info about what I'll be doing in the future

--- GUI/window.cpp
+++ GUI/window.cpp
@@ -1,3 +1,24 @@
 //USING wcWIDGETS FROM NOW on
 //Doing this for ease of use, it's widely used and understood, so
 //I think it's worth a try.
+
+//I'm going to define a couple things here, like the color pallete and stuff.
+
+//color palette
+//I'm following https://material.io/design/color/dark-theme.html#behavior
+// +=============================+=========+
+// |      Dark Mode GUI color palette      |
+// +=============================+=========+
+// | Primary (Background)        | #121212 |
+// +-----------------------------+---------+
+// | Primary (Light)             | #383838 |
+// +-----------------------------+---------+
+// | Surface                     | #272727 |
+// +-----------------------------+---------+
+// | Secondary                   | #76ff03 |
+// +-----------------------------+---------+
+// | Secondary Variant 1 (Light) | #b0ff57 |
+// +-----------------------------+---------+
+// | Secondary Variant 2 (Dark)  | #32cb00 |
+// +-----------------------------+---------+
+//I've put this here so I know what color to make the icons when I download them.
