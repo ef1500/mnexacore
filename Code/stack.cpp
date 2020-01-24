ebfa01bc5470094bc6d2554ac1fbed96e27875a4
ef1500 <45581646+ef1500@users.noreply.github.com>
2020-01-24 10:39:18 -0700

    Added the Stack integration files

---
+++ Code/stack.cpp
@@ -0,0 +1,17 @@
+//written by ef1500
+//This is the stack engine. It is designed to contron the
+//flow of cardds through the UI.
+
+//Declare header files (duh)
+#include <iostream>
+#include <vector>
+
+//Declare namespaces
+using namespace std;
+
+//Create vectors for the Stack
+void stackinit(vector<int>vx, int n)
+{
+  //make room for n amount of elements in the stack
+  vx.reserve(n);
+}
